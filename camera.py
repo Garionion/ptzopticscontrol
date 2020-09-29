70524644a95f10f49bd4e544438c7b4db694adb0
garionion <github@matthias-detsch.de>
2020-09-29 09:26:34 +0200

    add controler specific mapping toogle, add position of camera

--- camera.py
+++ camera.py
@@ -27,30 +27,41 @@ class Camera:
         zoomin="2",
         zoomout="3",
         zoomstop="0",
+        focusin="2",
+        focusout="3",
+        focusstop="0",
     )
 
-    def __init__(self, ip: str, port: int = 1259, defaults: dict = dict()):
+    def __init__(self, ip: str, port: int = 1259, defaults: dict = dict(), position=0):
         self.ip = ip
         self.defaults = {**self.defaults, **defaults}
         self.last_url = ''
         self.last_message = ''
+        self.last_move = ''
+        self.last_focus = ''
+        self.last_zoom = ''
         self.port = port
+        self.position = position
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 
-    def send(self, msg):
-        if msg == self.last_message:
+    def send(self, msg, force=False):
+        if msg == self.last_message and not force:
             return
+        logging.debug(f"send {msg}")
         self.sock.sendto(msg, (self.ip, self.port))
-        recvMsg = 0
         while True:
             nextBytes = self.sock.recv(50)
             recvMsg = int(nextBytes.hex(), 16)
+            
             if 9461759 >= recvMsg >= 9457919:
-                recvMsg = 0
                 continue
+            if 2423210495 >= recvMsg >= 2422211327:
+                logging.error(hex(recvMsg))
+                break
             elif 9457663 >= recvMsg >= 9453823:
                 self.last_message = msg
                 break
+        logging.info("Send finished")
 
     def _action(self, action: str, *args, **kwargs):
         url = f"http://{self.ip}//cgi-bin/ptzctrl.cgi?ptzcmd&{action}"
@@ -100,15 +111,48 @@ class Camera:
 
         act = Camera.actions.get(action, Camera.actions["stop"])
         msg = bytearray.fromhex(f"81 01 06 01 {toHex(panSpeed)} {toHex(tiltSpeed)} {act} FF")
+        if msg == self.last_move:
+            return
+        self.last_move = msg
         return self.send(msg)
+    
+    def focus(self, focusSpeed):
+        action = ''
 
-    def zoom(self, action, zoomspeed):
-        if zoomspeed == None:
-            zoomspeed = Camera.defaults["zoom_speed"]
+        if focusSpeed:
+            action = 'focusin' if focusSpeed > 0 else 'focusout'
+            focusSpeed = abs(focusSpeed)
+
+        if not action:
+            action = 'focusstop'
+
+        act = Camera.actions.get(action, Camera.actions.get(action, Camera.actions["focusstop"]))
+
+        msg = bytearray.fromhex(f"81 01 04 08 {act}{focusSpeed} FF")
+        if msg == self.last_focus:
+            return
+        print(f"focus {action} - {focusSpeed}")
+        self.last_focus = msg
+        self.send(msg)
+    
+    def zoom(self, zoomSpeed):
+        action = ''
+
+        if zoomSpeed:
+            action = 'zoomin' if zoomSpeed < 0 else 'zoomout'
+            zoomSpeed = abs(zoomSpeed)
+
+        if not action:
+            action = 'zoomstop'
+
         act = Camera.actions.get(action, Camera.actions.get(action, Camera.actions["zoomstop"]))
 
-        msg = bytearray.fromhex(f"81 01 04 07 {act}{zoomspeed} FF")
-        self.send(msg)
+        msg = bytearray.fromhex(f"81 01 04 07 {act}{zoomSpeed} FF")
+        if msg == self.last_zoom:
+            return 
+        self.last_zoom = msg
+        print(f"focus {action} - {zoomSpeed}")
+        return self.send(msg)
 
     def set_preset(self, preset: int):
         msg = bytearray.fromhex(f"81 01 04 3F 01 {toHex(preset)} FF")
@@ -117,6 +161,10 @@ class Camera:
     def call_preset(self, preset: int):
         msg = bytearray.fromhex(f"81 01 04 3F 02 {toHex(preset)} FF")
         self.send(msg)
+    
+    def toggleAF(self):
+        msg = bytearray.fromhex("81 01 04 38 10 FF")
+        self.send(msg, force=True)
 
 
 if __name__ == "__main__":
--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 import signal
 from xbox360controller import Xbox360Controller
+import threading
 import datetime
 import time
-import requests
+import string
 from camera import Camera
+import difflib
+from enum import Enum
 
-cam = Camera("192.168.1.113")
-secondCam = Camera("192.168.1.110")
+class CamPosition(Enum):
+    right = 1
+    left = 2
 
 
-zoomspeed = 5
+cam = Camera(ip="192.168.1.113", position=CamPosition.left)
+secondCam = Camera(ip="192.168.1.110", position=CamPosition.right)
+
+maxZoomSpeed = 7
+maxFocusSpeed= 3
 maxPanSpeed = 10
 maxTiltSpeed = 10
 
@@ -25,25 +33,41 @@ class Preset:
         self.position = pos
 
 def panTilt(axis: Axis):
-    panSpeed = int(axis.x*maxPanSpeed) if abs(axis.x) > 0.19 else 0
-    tiltSpeed = int(axis.y*maxTiltSpeed) if abs(axis.y) > 0.19 else 0
+    panSpeed = int(axis.x * maxPanSpeed) if abs(axis.x) > 0.19 else 0
+    tiltSpeed = int(axis.y * maxTiltSpeed) if abs(axis.y) > 0.19 else 0
     cam.move(panSpeed=panSpeed, tiltSpeed=tiltSpeed)
+    time.sleep(0.15)
+
+def focus(focus):
+    focusspeed = int(focus * maxFocusSpeed) if abs(focus) > 0.19 else 0
+    cam.focus(focusSpeed=focusspeed)
+
+def zoom(zoom):
+    zoomspeed = int(zoom * maxZoomSpeed) if abs(zoom) > 0.19 else 0
+    cam.zoom(zoomSpeed=zoomspeed)
 
 def zoomin(button):
-    cam.zoom("zoomin", zoomspeed=None)
-
+    cam.zoom(zoomSpeed=cam.defaults["zoom_speed"])
 
 def zoomout(button):
-    cam.zoom("zoomout", zoomspeed=None)
-
+    cam.zoom(zoomSpeed=cam.defaults["zoom_speed"])
 
 def zoomstop(button):
-    cam.zoom("zoomstop", zoomspeed=0)
+    cam.zoom(zoomSpeed=cam.defaults["zoom_speed"])
 
-def switchIP(button):
+def switchCam(button):
     global cam, secondCam
-    controller.set_rumble(1, 1, 80)
     cam, secondCam = secondCam, cam
+    if cam.position == CamPosition.right:
+        controller.set_rumble(0, 1, 200)
+    else:
+        controller.set_rumble(1, 0, 200)
+
+def toggleAF(button):
+    cam.toggleAF()
+    controller.set_rumble(1, 0, 120)
+    time.sleep(0.2)
+    controller.set_rumble(0, 1, 120)
     
 def handlePresetStart(button):
     global presets
@@ -60,12 +84,13 @@ def handlePreset(button):
         cam.call_preset(preset)
 
 
-def on_button_released(button):
-    print('Button {0} was released'.format(button.name))
 
 axis = Axis()
 lastUrl = ""
 
+
+def on_axis_moved(axis):
+    print('Axis {0} moved to {1} {2}'.format(axis.name, axis.x, axis.y))
 
 presets = {
     "button_a": Preset(1),
@@ -76,15 +101,21 @@ presets = {
 
 if __name__ == '__main__':
     try:
-        with Xbox360Controller(0, axis_threshold=0) as controller:
-
+        name = ''.join([x if x in string.printable else '' for x in Xbox360Controller.get_available()[0].name])
+        if name == "Logitech Logitech RumblePad 2 USB":
+            switchAxisR = True
+        else:
+            switchAxisR = False
+        print(switchAxisR)
+        with Xbox360Controller(0, axis_threshold=0, switch_axis_r_with_trigger_l=switchAxisR) as controller:
             # Button A events
             controller.button_trigger_l.when_pressed = zoomout
             controller.button_trigger_r.when_pressed = zoomin
             controller.button_trigger_l.when_released = zoomstop
             controller.button_trigger_r.when_released = zoomstop
 
-            controller.button_start.when_pressed = switchIP
+            controller.button_start.when_pressed = switchCam
+            controller.button_select.when_pressed = toggleAF
 
             controller.button_a.when_pressed = handlePresetStart
             controller.button_b.when_pressed = handlePresetStart
@@ -99,12 +130,20 @@ if __name__ == '__main__':
             # Left and right axis move event
             #controller.axis_l.when_moved = on_axis_moved
             #controller.axis_r.when_moved = on_axis_moved
+
             while True:
+                focusValue = controller.axis_r.x
+                zoomValue = controller.axis_r.y
 
                 axis.x = controller.axis_l.x
                 axis.y = controller.axis_l.y
                 panTilt(axis)
-                time.sleep(0.15)
+                focusValue = controller.axis_r.x
+                zoomValue = controller.axis_r.y
+
+                focus(focusValue)
+                zoom(zoomValue)
+                time.sleep(0.1)
             signal.pause()
     except KeyboardInterrupt:
         pass
